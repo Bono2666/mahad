f907f6143822179b3a26bfa42e5a0c93b0fa13ff
Bono Faradianto <trihambono@gmail.com>
2025-08-15 17:46:42 +0700

    First commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bono2666/Library/CloudStorage/OneDrive-Personal/Being Worked On/Al Ukhuwah/marbot/venv"
+set -gx VIRTUAL_ENV "/Users/bono2666/Downloads/mahad/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
